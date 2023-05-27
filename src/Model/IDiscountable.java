2197153be98c12a1ff3f9113e8abacd1051bed8a
Hossein Shirkavand <hosseinshirkavand82@gmail.com>
2023-05-27 15:24:42 +0330

    code optimized - no special changes

--- src/Model/IDiscountable.java
+++ src/Model/IDiscountable.java
@@ -1,11 +1,9 @@
 package Model;
 
-import Model.Stuffs.Stuff;
-
 public interface IDiscountable {
-    public void addDiscount(double discountPercent);
+    void addDiscount(double discountPercent);
 
-    default public void removeDiscount() {
+    default void removeDiscount() {
         addDiscount(0);
     }
 }
